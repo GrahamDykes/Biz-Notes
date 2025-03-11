ad131e83b71a8df9590f65e1ed8d74c37438b916
GrahamDykes <153469002+GrahamDykes@users.noreply.github.com>
2025-03-11 10:54:57 -0700

    make it end

--- Backend/config/allowedOrigins.js
+++ Backend/config/allowedOrigins.js
@@ -1,7 +1,14 @@
 const allowedOrigins = [
     'http://localhost:3000',
+    'http://localhost:3500',
+    'http://localhost:5000',
+    'https://localhost:3000',
+    'https://localhost:3500',
+    'https://localhost:5000',
     'https://www.dandrepairshop.com',
-    'https://dandrepairshop.com'
+    'https://dandrepairshop.com',
+    'http://194.163.45.216',
+    'https://194.163.45.216',
 ]
 
 module.exports = allowedOrigins
