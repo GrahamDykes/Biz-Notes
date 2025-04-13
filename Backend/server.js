bdb1dcf811f6861cdda5ee35ff9abba3e3e96e7a
GrahamDykes <grahamdykes@gmail.com>
2025-04-13 15:56:28 -0700

    ch ch ch changes

--- Backend/server.js
+++ Backend/server.js
@@ -49,6 +49,7 @@ mongoose.connection.once('open', () => {
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
 })
 
+
 mongoose.connection.on('error', err => {
     console.log(err)
     logEvents(`${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`, 'mongoErrLog.log')
